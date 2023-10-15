d11e391994ccee9ea7ec8d36fedcaa320c1ce035
Marko Ilic <19marko91.ilic@gmail.com>
2023-10-15 19:22:16 +0200

    First commit Ecommerce app

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,70 @@
+package com.luv2code.ecommerce.config;
+
+import com.luv2code.ecommerce.model.Product;
+import com.luv2code.ecommerce.model.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    //using this config we are setting up those http methods to be unsupported, only GET
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
+
+        //disable for Product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //disable for Product category
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //call method to expose ids
+        exposeIds(config);
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        //expose entity ids
+        //get list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        //create array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        //get the types for the entities
+        for (EntityType tempType : entities) {
+            entityClasses.add(tempType.getJavaType());
+        }
+
+        // expose ids for the array of entity
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+}
---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/ProductCategoryService.java
@@ -0,0 +1,8 @@
+package com.luv2code.ecommerce.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductCategoryService {
+
+}
